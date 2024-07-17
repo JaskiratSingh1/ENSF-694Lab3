c1ebc3e8ed86f3af573425bdb050b72e3f9de86e
Jaskirat Singh <jaskirat1999@gmail.com>
2024-07-16 21:39:57 -0600

    Completed exercise E

--- exerciseE/DynamicStack.cpp
+++ exerciseE/DynamicStack.cpp
@@ -8,60 +8,128 @@
 #include "DynamicStack.h"
 
 DynamicStack::DynamicStack( int n ){
-    // STUDENTS NEED TO COMPLETE OR CORRECT THE IMPLEMENTATION OF THIS FUNTION
+    entry = 0;
+    initial_capacity = current_capacity = n;
+    array = new int[initial_capacity];
 }
 
 
 DynamicStack::DynamicStack( DynamicStack const &stack ) {
-    // STUDENTS NEED TO COMPLETE OR CORRECT THE IMPLEMENTATION OF THIS FUNTION
+    entry = stack.entry;
+    initial_capacity = stack.initial_capacity;
+    current_capacity = stack.current_capacity;
+    array = new int[current_capacity];
+    for (int i = 0; i < entry; i++){
+        array[i] = stack.array[i];
+    }
 }
 
 
 DynamicStack::~DynamicStack() {
-    // STUDENTS NEED TO COMPLETE OR CORRECT THE IMPLEMENTATION OF THIS FUNTION
+    delete[] array;
 }
 
 
 int DynamicStack::top() const {
-    // STUDENTS NEED TO COMPLETE OR CORRECT THE IMPLEMENTATION OF THIS FUNTION
-    return NULL; // WRONG RETRUN VALUE
+    //Check if stack is empty
+    if(entry > 0) {
+        return array[entry - 1];
+    }
+    //Stack is empty
+    return -1;
 }
 
 int DynamicStack::size() const {
-    // STUDENTS NEED TO COMPLETE OR CORRECT THE IMPLEMENTATION OF THIS FUNTION
-    return NULL; // WRONG RETRUN VALUE
+    //Entry holds the location for the enxt element, i.e. size
+    return entry;
 }
 
 
 bool DynamicStack::empty() const {
-    // STUDENTS NEED TO COMPLETE OR CORRECT THE IMPLEMENTATION OF THIS FUNTION
-    return true;
+    //If entry is 0 then stack is empty
+    return entry == 0;
 }
 
 int DynamicStack::capacity() const {
-    // STUDENTS NEED TO COMPLETE OR CORRECT THE IMPLEMENTATION OF THIS FUNTION
-    return NULL; // WRONG RETRUN VALUE
+    return current_capacity;
 }
 
 DynamicStack &DynamicStack::operator=( DynamicStack const &rhs ) {
-    // STUDENTS NEED TO COMPLETE OR CORRECT THE IMPLEMENTATION OF THIS FUNTION
+    //Copy constructor
+    if(this != &rhs) {
+        delete[] array;
+        entry = rhs.entry;
+        initial_capacity = rhs.initial_capacity;
+        current_capacity = rhs.current_capacity;
+        array = new int[current_capacity];
+        for(int i = 0; i < entry; i++) {
+            array[i] = rhs.array[i];
+        }
+    }
     return *this;
 }
 
-
 void DynamicStack::push(const int  &obj ) {
-    // STUDENTS NEED TO COMPLETE OR CORRECT THE IMPLEMENTATION OF THIS FUNTION
+    if(entry == current_capacity){
+        //Double capacity if it has been reached
+        current_capacity *= 2;
+        //Generate new array with new capacity
+        int *newArray = new int[current_capacity];
+        //Populate new array
+        for(int i = 0; i < entry; i++) {
+            newArray[i] = array[i];
+        }
+        //Delete old array
+        delete[] array;
+        //Point member array to new enlarged array
+        array = newArray;
+    }
+    //Add the new element in the stack
+    array[entry++] = obj;
 }
 
 int DynamicStack::pop() {
-    // STUDENTS NEED TO COMPLETE OR CORRECT THE IMPLEMENTATION OF THIS FUNTION
-    return NULL; // WRONG RETRUN VALUE
+    //If array is empty do nothing
+    if (entry == 0) {
+        return -1;
+    }
+    //Get new top of stack, decrementing entry
+    int value = array[--entry];
+    
+    //Halve current capacity if the stated conditions are met
+    if(entry == current_capacity / 4 && current_capacity > initial_capacity && entry > 0){
+        //Halve the capacity
+        current_capacity /= 2;
+        //Create new array with new capacity
+        int *newArray = new int[current_capacity];
+        //Populate new array
+        for (int i = 0; i < entry; ++i) {
+            newArray[i] = array[i];
+        }
+        //Delete old array in memory
+        delete[] array;
+        //Point member array to new smaller array
+        array = newArray;
+    }
+    //Return the value of the new top of stack
+    return value;
 }
 
-void DynamicStack::clear() {
-
+void DynamicStack::clear(){
+    //Delete the existing array in memory
+    delete[] array;
+    //Reset current capcity to initial value
+    current_capacity = initial_capacity;
+    //Create new array with the new current capacity
+    array = new int[current_capacity];
+    //Reset entry to 0
+    entry = 0;
 }
 
 void DynamicStack::display(){
- 
+    //Print out all the entries of the stack
+    for(int i = 0; i < entry; i++){
+        cout << array[i] << " ";
+    }
+    cout << endl;
 }
